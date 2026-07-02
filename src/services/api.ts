f7bbeb839bc352c070cae0ca7f82824d80783305
Abdullah AlMalki <108226495+aalmalki3@users.noreply.github.com>
2026-07-03 00:41:54 +0300

    Update API base URL to a new endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios'
 import { useAuthStore } from '../store/authStore'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ||
-  'https://script.google.com/macros/s/AKfycbwt-2Bdu0cgnB1z3ev0U5pbd1pPSSNp7u2q77m4N9Us_VKNvNuNNTdzbkbr0MebTWSU/exec'
+  'https://script.google.com/macros/s/AKfycbyMqqUQCBklnNjITRU6bQcwBVU-PbmHU5CmvG_OvkXfI4z9OV5DHD2IjEef1oYdIdqg/exec'
 
 const rawClient = axios.create({
   baseURL: API_BASE_URL,
