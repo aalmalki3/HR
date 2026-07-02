3d71fdfe608d379ea2ebef10335c0ea59c891b27
Abdullah AlMalki <108226495+aalmalki3@users.noreply.github.com>
2026-07-03 00:23:29 +0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, AxiosError } from 'axios'
 import { useAuthStore } from '../store/authStore'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 
-  'https://script.google.com/macros/d/YOUR_GAS_DEPLOYMENT_ID/usercallback'
+  'https://script.google.com/macros/s/AKfycbwt-2Bdu0cgnB1z3ev0U5pbd1pPSSNp7u2q77m4N9Us_VKNvNuNNTdzbkbr0MebTWSU/exec'
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
